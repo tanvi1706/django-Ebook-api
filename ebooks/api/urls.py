d4e3665598f545846f853ebe623343478eb88453
tanvi1706 <tanvikrishnapatil@tanvis-MBP.lan>
2021-05-19 22:20:29 -0500

    v 1.2

--- ebooks/api/urls.py
+++ ebooks/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from ebooks.api.view import EbookListCreateAPIView
+from ebooks.api.view import EbookListCreateAPIView, EbookDetailAPIView
 
 urlpatterns =[
     path('ebooks/', EbookListCreateAPIView.as_view(), name="ebook-list"),
+    path('ebooks/<int:pk>/', EbookDetailAPIView.as_view(), name="ebook-detail"),
 ]
--- ebooks/api/view.py
+++ ebooks/api/view.py
@@ -4,13 +4,22 @@ from rest_framework import mixins
 from ebooks.models import Ebook
 from ebooks.api.serializers import EbookSerializer
 
-class EbookListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+
+class EbookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+# class EbookListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
         
